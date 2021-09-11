b76d0b87b05cf96fe0cc1ce56092920295c743fb
Patrick <patasax22@yahoo.com>
2021-09-11 13:46:36 -0400

    task-persistance added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var now = moment();
 var currentTime = now.hour();
 var timeBlock = $(".time-block");
 var describe = $(".description");
+var saveBtn = $(".saveBtn")
 
 var displayToday = function() {
     today = moment(now).format("dddd, MMMM Do");
@@ -10,15 +11,18 @@ var displayToday = function() {
 
 setInterval(displayToday, 1000);
 
+// pulls event information from local storage as well as coloring timeblocks to indicate past, present, or future
 var whatTime = function() {
     // figure what time it is
     timeBlock.each(function() {
         // get time-blocks from index file
         var id = parseInt($(this).attr("id"));
-        var task = JSON.parse(localStorage.getItem(id));
+        // pulls info from localStorage
+        var task = localStorage.getItem(id);
         if (task != "null") {
             $(this).find(describe).val(task);
         };
+        // iterates to determine coloring
         if (id === currentTime) {
             $(this).addClass("present");
         } else if (id < currentTime) {
@@ -27,10 +31,16 @@ var whatTime = function() {
             $(this).addClass("future")
         };
     });
-    // figure which time-blocks are <, >, or = using for loop
-    // then dynamically display the corrisponding color for the given class (past, present, or future)
 };
 
-
+// function to save tasks/events on button click
+saveBtn.on("click", function() {
+    // defines storage key
+    var hour = $(this).parent().attr("id");
+    // defines data to be stored
+    var task = $(this).siblings().eq(1).val();
+    // sets data to localStorage
+    localStorage.setItem(hour, task);
+});
 
 whatTime();
