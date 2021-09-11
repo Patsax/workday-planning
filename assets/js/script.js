bd89d81ca142db655de4302d588184379798f0df
Patrick <patasax22@yahoo.com>
2021-09-11 13:02:05 -0400

    time-based coloration added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,36 @@
-const dateFormat = "dddd, MMMM Do"
 var now = moment();
+var currentTime = now.hour();
+var timeBlock = $(".time-block");
+var describe = $(".description");
 
 var displayToday = function() {
-    today = moment(now).format(dateFormat);
+    today = moment(now).format("dddd, MMMM Do");
     document.getElementById("currentDay").innerHTML = today;
 };
 
-displayToday();
+setInterval(displayToday, 1000);
+
+var whatTime = function() {
+    // figure what time it is
+    timeBlock.each(function() {
+        // get time-blocks from index file
+        var id = parseInt($(this).attr("id"));
+        var task = JSON.parse(localStorage.getItem(id));
+        if (task != "null") {
+            $(this).find(describe).val(task);
+        };
+        if (id === currentTime) {
+            $(this).addClass("present");
+        } else if (id < currentTime) {
+            $(this).addClass("past")
+        } else {
+            $(this).addClass("future")
+        };
+    });
+    // figure which time-blocks are <, >, or = using for loop
+    // then dynamically display the corrisponding color for the given class (past, present, or future)
+};
+
+
+
+whatTime();
